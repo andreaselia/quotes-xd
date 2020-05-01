363578d278467ea49ff6f30c84d575ce58938ac5
Andreas Elia <andreaselia@live.co.uk>
2020-05-01 15:30:49 +0100

    new context menu and generator functionality

--- main.js
+++ main.js
@@ -7,26 +7,66 @@ async function randomQuote() {
     return data;
 }
 
-async function quoteHandlerFunction(selection) {
+async function quickQuoteHandlerFunction(selection) {
     try {
         const quote = await randomQuote();
-
-        const node = new Text();
-        node.text = `${quote.content} - ${quote.author}`;
-        node.fill = new Color().clone();
-        node.fontSize = 18;
-        node.areaBox = { width: 800, height: 200 };
-
-        selection.insertionParent.addChild(node);
-        node.moveInParentCoordinates(20, 20);
     } catch (ex) {
-        console.log("Detailed error: ", ex);
-        await error("Oops", "Please check your network connection.");
+        await error("Please check your network connection.");
+        return;
+    }
+
+    const node = new Text();
+    node.text = `${quote.content} - ${quote.author}`;
+    node.fill = new Color().clone();
+    node.fontSize = 18;
+    node.areaBox = { width: 800, height: 200 };
+
+    selection.insertionParent.addChild(node);
+    node.moveInParentCoordinates(20, 20);
+}
+
+async function fillQuoteHandlerFunction(selection) {
+    let quote = '';
+
+    try {
+        quote = await randomQuote();
+    } catch (ex) {
+        await error("Please check your network connection.");
+        return;
+    }
+
+    let candidateItems = 0;
+    let affectedItems = 0;
+
+    selection.items.filter(item => item instanceof Text)
+        .forEach(item => {
+            candidateItems++;
+
+            const text = item.text;
+            const length = text.length;
+
+            item.text = `${quote.content} - ${quote.author}`;
+
+            affectedItems++;
+        });
+
+    if (candidateItems === 0 || affectedItems === 0) {
+        if (selection.items.length === 0 || candidateItems === 0) {
+            alert(selection.items.length === 0 ? "Nothing selected..." : "No operable items selected...", "Be sure to select one or more text items.");
+            return;
+        }
+
+        if (candidateItems > affectedItems) {
+            error("No operable items selected...");
+            return;
+        }
     }
 }
 
 module.exports = {
     commands: {
-        getQuote: quoteHandlerFunction
+        quickQuote: quickQuoteHandlerFunction,
+        fillQuote: fillQuoteHandlerFunction,
+        shuffleQuote: fillQuoteHandlerFunction
     }
 };
